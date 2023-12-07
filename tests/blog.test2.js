873acd61b07f8a146303e03961fee3d64c559faf
s6101012620148 <tamkmutnb@gmail.com>
2023-12-07 20:06:30 +0700

    Made create and update method

--- tests/blog.test2.js
+++ tests/blog.test2.js
@@ -3,9 +3,9 @@ import http from "k6/http"
 export const options = {
     discardResponseBodies: true,
     scenarios: {
-        contacts: {
+        userCreateBlog: {
             executor: "constant-vus",
-            exec: "contacts",
+            exec: "createBlog",
             vus: 50,
             duration: "30s",
         },
@@ -20,6 +20,71 @@ export const options = {
     },
 }
 
+export function crateBlog() {
+    // Generate a random blog object for testing
+    let blog = {
+        // Add your blog properties here
+        postId: 0,
+        postTitle: "testTitle",
+        postDetail: "testDetail",
+        postDtm: "2023-12-02",
+        postAuthor: "tester",
+        postStatus: 1,
+        // Add other required properties
+    }
+    // Send a POST request to create a new blog
+    let createResponse = http.post(
+        "http://localhost:3000/blog/create",
+        JSON.stringify(blog),
+        {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }
+    )
+    // Check if the blog creation was successful (status code 200)
+    check(createResponse, {
+        "Create Blog Status is 200": (res) => res.status === 200,
+    })
+    // Sleep for a short duration before sending the next request
+    sleep(1)
+}
+
+export function updateBlog() {
+    // Define the base URL for your API
+    const baseUrl = "http://localhost:3000"
+
+    // Generate a random postId in the range of 1-4400 for testing
+    const postId = Math.floor(Math.random() * 4400) + 1
+
+    // Define the payload for the update request
+    const payload = {
+        postTitle: "Updated",
+        postDetail: "Updated",
+        postDtm: "2023-12-07",
+        postAuthor: "tester",
+        postStatus: 1,
+    }
+
+    // Send the update request
+    const updateResponse = http.put(
+        `${baseUrl}/update/${postId}`,
+        JSON.stringify(payload),
+        {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }
+    )
+
+    // Check if the update was successful (status code 200)
+    check(updateResponse, {
+        "Update successful": (resp) => resp.status === 200,
+    })
+
+    // Add a sleep to simulate user think time
+    sleep(1)
+}
 export function contacts() {
     http.get("https://test.k6.io/contacts.php", {
         tags: { my_custom_tag: "contacts" },
